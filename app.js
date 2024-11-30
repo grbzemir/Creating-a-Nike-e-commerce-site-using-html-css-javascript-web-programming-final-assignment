6146f6627ccc52dc4c8e4999c8e4ad0c03e1a121
grbzemir <eyetkingurbuz@trakya.edu.tr>
2024-11-30 16:14:33 +0300

    change

--- app.js
+++ app.js
@@ -1,16 +1,125 @@
-const wrapper = document.querySelector('.sliderWrapper');
-const menuItems = document.querySelectorAll('.menuItem');
+const wrapper = document.querySelector(".sliderWrapper");
+const menuItems = document.querySelectorAll(".menuItem");
+
+const products = [
+    {
+        id: 1,
+        title: "Air Force",
+        price: 119,
+        colors: [
+            {
+                code: "black",
+                img: "./image/air.png",
+            },
+            {
+                code: "darkblue",
+                img: "./image/air2.png",
+            },
+        ],
+    },
+    {
+        id: 2,
+        title: "Air Jordan",
+        price: 149,
+        colors: [
+            {
+                code: "lightgray",
+                img: "./image/jordan.png",
+            },
+            {
+                code: "green",
+                img: "./image/jordan2.png",
+            },
+        ],
+    },
+    {
+        id: 3,
+        title: "Blazer",
+        price: 109,
+        colors: [
+            {
+                code: "lightgray",
+                img: "./image/blazer.png",
+            },
+            {
+                code: "green",
+                img: "./image/blazer2.png",
+            },
+        ],
+    },
+    {
+        id: 4,
+        title: "Crater",
+        price: 129,
+        colors: [
+            {
+                code: "black",
+                img: "./image/crater.png",
+            },
+            {
+                code: "lightgray",
+                img: "./image/crater2.png",
+            },
+        ],
+    },
+    {
+        id: 5,
+        title: "Hippie",
+        price: 99,
+        colors: [
+            {
+                code: "gray",
+                img: "./image/hippie.png",
+            },
+            {
+                code: "black",
+                img: "./image/hippie2.png",
+            },
+        ],
+    },
+];
+
+let choosenProduct = products[0];
+
+const currentProductImg = document.querySelector(".productImg");
+const currentProductTitle = document.querySelector(".productTitle");
+const currentProductPrice = document.querySelector(".productPrice");
+const currentProductColors = document.querySelectorAll(".color");
+const currentProductSizes = document.querySelectorAll(".size");
 
 menuItems.forEach((item, index) => {
-
-    item.addEventListener('click', () => {
-        // console.log("you clicked!" + index);
+    item.addEventListener("click", () => {
+        //ayakabıyı değiştirme
         wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+        //ürünü değiştirme
+        choosenProduct = products[index];
+
+        //ürünün açıklamasını parasını fotografını değiştirme
+        currentProductTitle.textContent = choosenProduct.title;
+        currentProductPrice.textContent = "$" + choosenProduct.price;
+        currentProductImg.src = choosenProduct.colors[0].img;
+
+        //yeni renk
+        currentProductColors.forEach((color, index) => {
+            color.style.backgroundColor = choosenProduct.colors[index].code;
+        });
     });
 });
 
+currentProductColors.forEach((color, index) => {
+    color.addEventListener("click", () => {
+        currentProductImg.src = choosenProduct.colors[index].img;
+    });
+});
 
-
-// console.log(wrapper);
-// wrapper.style.backgroundColor = 'red';
-// wrapper.style.transform = 'translateX(-300vw)';
+currentProductSizes.forEach((size, index) => {
+    size.addEventListener("click", () => {
+        currentProductSizes.forEach((size) => {
+            size.style.backgroundColor = "white";
+            size.style.color = "black";
+        });
+        size.style.backgroundColor = "black";
+        size.style.color = "white";
+    });
+});
